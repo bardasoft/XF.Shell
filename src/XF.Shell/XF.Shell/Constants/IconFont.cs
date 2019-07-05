8545c38b7eff1a78f457cea98078598b98716330
Chris Howarth <chowarth23@gmail.com>
2019-07-05 21:06:46 +0100

    Add more content to the FontIcon page
    Added examples of different ways to use the icons e.g. x:Static, Binding or FontImageSource

--- src/XF.Shell/XF.Shell/Constants/IconFont.cs
+++ src/XF.Shell/XF.Shell/Constants/IconFont.cs
@@ -3,10 +3,47 @@ namespace XF.Shell.Constants
 {
     public static class IconFont
     {
-        public const string AccountBox = "\uf006";
-        public const string GithubBox = "\uf2a3";
-        public const string Microsoft = "\uf372";
-        public const string Twitch = "\uf543";
-        public const string Xamarin = "\uf844";
+        public static string Numeric0 = "\u0030";
+        public static string Numeric1 = "\u0031";
+        public static string Numeric2 = "\u0032";
+        public static string Numeric3 = "\u0033";
+        public static string Numeric4 = "\u0034";
+        public static string Numeric5 = "\u0035";
+        public static string Numeric6 = "\u0036";
+        public static string Numeric7 = "\u0037";
+        public static string Numeric8 = "\u0038";
+        public static string Numeric9 = "\u0039";
+        public static string AlphaA = "\u0041";
+        public static string AlphaB = "\u0042";
+        public static string AlphaC = "\u0043";
+        public static string AlphaD = "\u0044";
+        public static string AlphaE = "\u0045";
+        public static string AlphaF = "\u0046";
+        public static string AlphaG = "\u0047";
+        public static string AlphaH = "\u0048";
+        public static string AlphaI = "\u0049";
+        public static string AlphaJ = "\u004a";
+        public static string AlphaK = "\u004b";
+        public static string AlphaL = "\u004c";
+        public static string AlphaM = "\u004d";
+        public static string AlphaN = "\u004e";
+        public static string AlphaO = "\u004f";
+        public static string AlphaP = "\u0050";
+        public static string AlphaQ = "\u0051";
+        public static string AlphaR = "\u0052";
+        public static string AlphaS = "\u0053";
+        public static string AlphaT = "\u0054";
+        public static string AlphaU = "\u0055";
+        public static string AlphaV = "\u0056";
+        public static string AlphaW = "\u0057";
+        public static string AlphaX = "\u0058";
+        public static string AlphaY = "\u0059";
+        public static string AlphaZ = "\u005a";
+        public static string Xamarin = "\uf844";
+        public static string Microsoft = "\uf372";
+        public static string Github = "\uf2a4";
+        public static string Android = "\uf032";
+        public static string Apple = "\uf035";
+        public static string Twitch = "\uf543";
     }
 }
--- src/XF.Shell/XF.Shell/ViewModels/FontIconPageViewModel.cs
+++ src/XF.Shell/XF.Shell/ViewModels/FontIconPageViewModel.cs
@@ -1,13 +1,21 @@
 ﻿
+using System.Linq;
+using System.Reflection;
 using XF.Shell.Constants;
 
 namespace XF.Shell.ViewModels
 {
     public class FontIconPageViewModel
     {
-        public string GithubIcon
+        public string AndroidIcon => IconFont.Android;
+        public string AppleIcon => IconFont.Apple;
+        public string TwitchIcon => IconFont.Twitch;
+
+        public string[] Icons { get; private set; }
+
+        public FontIconPageViewModel()
         {
-            get => IconFont.GithubBox;
+            Icons = typeof( IconFont ).GetFields( BindingFlags.Static | BindingFlags.Public ).Select( x => x.GetValue( null ) as string ).ToArray();
         }
     }
 }
